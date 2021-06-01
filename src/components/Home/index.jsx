30446b46e9bdfbc7793df3d8917ced5ffd5ab9a4
Your Name <yo@gmail.com>
2021-06-01 18:47:53 -0500

    aprendiendo

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 export const Home = () => {
     return (
-        <div>
+        <Container>
             <h1>hola Home</h1>
             <Link to='/login'>Login</Link>
             <br></br>
             <Link to='/naruto'>naruto</Link>
             <br></br>
             <Link to='/hinata'>hinata</Link>
-        </div>
+
+                <Component title='titulo' span='Hola esto es un span' paragraph='parrafo'/>
+
+        </Container>
     )
 }
-
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+const ContainerText = styled.div`
+`
+const Component = ({ title, span, paragraph}) => (
+    <ContainerText>
+        <h2>{title}</h2>
+        <span>{span}</span>
+        <p>{paragraph}</p>
+    </ContainerText>
+)
--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,24 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import './styles.css'
  const Layout = props => {
     return (
         <div>
-            <header >
-                <nav>
-                    <ul>
+            <Header>
+                <Nav>
+                    <Ul>
                         <li>
                             <Link to='/'>Home</Link>
                             <Link to='/hinata'>Hinata</Link>
                             <Link to='/naruto'>ñe</Link>
                             <Link to='/login'>Login</Link>
+                            <Enlace to='/'>Enlace de prueba</Enlace>
                         </li>
-                    </ul>
-                </nav>
-            </header>
+                    </Ul>
+                </Nav>
+            </Header>
                 {props.children}
-            <footer className='panocha'>Footer</footer>
+            <footer>Footer</footer>
         </div>
     )
 }
+export const Nav = styled.nav`
+    display: flex;
+    margin: 10px;
+`
+export const Enlace = styled(Link)`
+    margin: 0px 10px 10px;
+    border: 30px solid green;
+    border-top: 30px solid yellow;
+    border-bottom: 30px solid yellow;
+`
+export const Header = styled.header`
+    display: flex;
+    box-shadow:  1px 1px 1px red;
+    background-color: red;
+    transition: .4s ease; 
+    // aqui aplicamos los estilos en el modo movile
+    @media(min-width: 769px){
+        background-color: yellow;
+    }
+    &:hover{
+        background-color: blue;
+
+    }
+`
+
+export const Ul = styled.ul`
+    display: flex;
+`
 export default Layout 
